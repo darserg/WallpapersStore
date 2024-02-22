09fa7fdf6ebb1622e178788bb519b722a04f8ff8
Darserg <Darserg1212>
2024-02-22 16:36:16 +0400

    Initial Commit

---
+++ SwiftUI-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Test
+//
+//  Created by Сергей Дарьин on 22.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI-Test
+//
+//  Created by Сергей Дарьин on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
