28d87d6f4673d0adfbbaf79a30406556c2471e28
Darserg <Darserg1212>
2024-03-16 16:14:00 +0400

    Some changes

---
+++ SwiftUI-Test/WallpaperDetail.swift
@@ -0,0 +1,18 @@
+//
+//  WallpaperDetail.swift
+//  SwiftUI-Test
+//
+//  Created by Сергей Дарьин on 02.03.2024.
+//
+
+import SwiftUI
+
+struct WallpaperDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WallpaperDetail()
+}
